ae956217f9c1ec7dd131f917f7825721963ea604
Martin Macharia <mosmartin5@gmail.com>
2019-06-23 21:13:47 +0200

    Update app.js
    Add the schema.

--- app.js
+++ app.js
@@ -1,8 +1,35 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const graphqlHTTP = require("express-graphql");
+const { buildSchema } = require("graphql");
 
 const app = express();
 
+// body parser middleware
 app.use(bodyParser.json());
 
-app.listen(3000);
+// graphql middleware
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {},
+    graphiql: true
+  })
+);
+
+app.listen(4000);
+console.log("Running a GraphQL API server at localhost:4000/graphql");
